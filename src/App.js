6dfb236631bbdfa3d563476ab0f8ce0985605db7
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-29 11:29:12 -0700

    built out Auth component to be rendered when website is pulled up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Todos from './components/Todos';
+import Auth from './components/Auth';
+import { useState } from 'react';
+import { getUser } from '../services/auth';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+
 
 function App() {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
   return (
     <div className="App">
-    
+      <Header user={user} setUser={setUser} />
+      <Switch>
+        <Route path="/auth/:type" component={Auth} />
+        <Route path="/todos" component={Todos} />
+        <Route exact path="/">
+          {!user && <Redirect to="/auth/sign-in" />}
+          {user && <Redirect to="/todos" />}
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/Components/Auth/Auth.js
@@ -0,0 +1,44 @@
+import { useContext } from 'react';
+import { NavLink, Redirect, useParams } from '"react-router-dom"';
+import { UserContext } from '../context/userContext';
+import { authUser } from '../services/auth';
+
+export default function Auth() {
+  const { type } = useParams();
+  const { user, setUser } = useContext(UserContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    const response = await authUser(email, password, type);
+    setUser(response);
+    e.target.reset();
+  };
+
+  if (user) {
+    return <Redirect to='/todos' />;
+  }
+
+  return (
+    <div className="Authentication">
+      <ul>
+        <li>
+          <NavLink to="/auth/sign-in">Sign In</NavLink>
+        </li>
+        <li>
+          <NavLink to="/auth/sign-up">Sign Up</NavLink>
+        </li>
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <button type="submit">{type}Submit</button>
+      </form>
+    </div>
+  );
+}
